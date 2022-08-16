65b7ff619b3fad2e3024b4d161fcb9849df414e2
App Soulz <simply.aakif@gmail.com>
2022-08-16 16:47:03 +0500

    Evaluation Chart Also Added

--- app/Filament/Resources/CandidateEvaluationResource.php
+++ app/Filament/Resources/CandidateEvaluationResource.php
@@ -41,6 +41,7 @@ class CandidateEvaluationResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('user.name')->searchable(),
+                Tables\Columns\TextColumn::make('user.candidate.mobile'),
                 Tables\Columns\TextColumn::make('course.course_name'),
                 Tables\Columns\TextColumn::make('candidate_evaluation_score')->label('Score'),
                 Tables\Columns\TextColumn::make('course.course_mode')->searchable(),
