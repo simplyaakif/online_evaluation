aa0ee8ec06f664b8f9625915247ddb1c6bb8f6fa
Aakif Raza <simply.aakif@gmail.com>
2023-12-01 23:07:49 +0500

    Price Issue Fixed

--- app/Filament/Resources/CourseResource/RelationManagers/SessionDurationsRelationManager.php
+++ app/Filament/Resources/CourseResource/RelationManagers/SessionDurationsRelationManager.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\CourseResource\RelationManagers;
 
 use App\Models\SessionDuration;
+use DB;
 use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
@@ -11,7 +12,9 @@ use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasRelationshipTable;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SessionDurationsRelationManager extends RelationManager
@@ -45,10 +48,31 @@ class SessionDurationsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                Tables\Actions\CreateAction::make()
+                    ->using(function (HasRelationshipTable $livewire, array $data): Model {
+//                        dd($livewire->ownerRecord->id,$data);
+                        DB::table('course_session_duration')->insert([
+                                                       'session_duration_id' => $data['session_duration_id'],
+                                                       'course_id' => $livewire->ownerRecord->id,
+                                                       'price'=>$data['price']
+                                                   ]);
+                        return $livewire->getRelationship()->latest()->first();
+                        //                        return $livewire->session()->create($data);
+                    }),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->using(function (Model $record, array $data): Model {
+//                        dd($record,$data);
+                        DB::table('course_session_duration')
+                            ->where('course_id', $record->course_id)
+                            ->where('session_duration_id', $record->session_duration_id)
+                            ->update([
+                                'price' => $data['price'],
+                                'session_duration_id' => $data['session_duration_id'],
+                                     ]);
+                        return $record;
+                    }),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
